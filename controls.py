12c47011800be2aac7fad08acef4ef7274641868
kemal <kemalharunkara@gmail.com>
2023-03-31 12:21:34 +0300

    dosya adı controls yapıldı kontrol işlemleri bitti

---
+++ controls.py
@@ -0,0 +1,17 @@
+print(10 > 9)
+print(10 == 9)          #true false değer döndürme
+print(10 < 9)
+
+a=10
+b=6
+if a > b:
+    print("a büyüktür")
+else:
+    print("b büyüktür")
+
+print(bool("Hello"))    #değer doluysa true boş ise false döndürür
+print(bool(15))
+
+x = 0
+print(isinstance(x, int)) # veri tipi kontrol eder
+
