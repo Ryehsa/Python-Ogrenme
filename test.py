30b1c8263cee862de355bf2c1e53acee092cfbdf
kemal <kemalharunkara@gmail.com>
2023-03-31 10:47:45 +0300

    dosyalar oluşturuldu

---
+++ test.py
@@ -0,0 +1,19 @@
+print("kemal")
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y, z) # aralarında 1 er boşluk bırakarak yazdırma
+print(x + y + z) # boşluk bırakmadan yazdırma
+a=5
+b=5
+print(a,b)
+print(type(b))# değişken veri türünü öğrenme
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
