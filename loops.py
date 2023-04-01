63789e25f1d5ef7f65f89e18ae777a2fa319225a
kemal <kemalharunkara@gmail.com>
2023-04-02 00:38:31 +0300

    for ve if kullanımı fonksiyonlardan devam edilicek

--- loops.py
+++ loops.py
@@ -1,7 +1,26 @@
 i=0
-while i < 6:
+while i < 2:
   print(i)
+  i=i+1
   
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
+  if x == "banana":
+    break
   print(x)
+  
+for x in range(2,6): # 2 6 arası yazdır
+  print(x)
+
+for x in range(2, 10, 3): # 2 den 30 a kadar 3 arttırarak yazdıor
+  print(x)
+  
+
+
+for x in range(6):
+  if x == 3: 
+    break
+  print(x)
+else:
+  print("Finally finished!")
+
---
+++ uygulama.py
@@ -0,0 +1,22 @@
+#büyük sayi
+i=5
+b=2
+if i>b:
+    print("i buyuk")
+elif i==b:
+    print("sayilar esit")
+else: 
+    ("b buyuk")
+
+#liste içinde büyük sayi bulma 
+sayilar=[1,2,3,4,5,6]
+a=sayilar[0]
+for x in sayilar:
+    if a<x:
+        a=x
+else:
+    print(a)
+
+
+sayi=input("sayi giriniz ")
+print(sayi)
