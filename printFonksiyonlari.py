c5bffeb1fab69b3d3dbbbef1425bf1aa7cbfe8d3
kemal <kemalharunkara@gmail.com>
2023-04-03 14:05:02 +0300

    print foksiyonları denendi

---
+++ printFonksiyonlari.py
@@ -0,0 +1,11 @@
+print("http://", "www.", "istihza.", "com")
+print("T", "C","K", sep=".")  # elemnların arasına metin ekler 
+print("Bugün günlerden Salı", end=".") # sona metin ekler
+
+print(*"Linux", sep=".") # * her öğeyi ayırarak yazar
+len("Galatasaray") 
+
+print ("test \"piton\"") # "" içinde metin yazdırıcaksan \ kullanılır
+
+a=5
+print(complex(a))
