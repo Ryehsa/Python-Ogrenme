8dffe6347fa0766532e0cf46e3972de26063a6fa
kemal <kemalharunkara@gmail.com>
2023-04-04 11:41:14 +0300

    apiler denendi hafta sonu bakılmaya devam edilcek

--- requestsDeneme.py
+++ requestsDeneme.py
@@ -1,7 +1,35 @@
 import requests
-r = requests.get('https://api.github.com/events', stream=True)
-r.raw
-r.raw.read(10)
+import json
 
-with open('ResultHeaders.txt', 'rb') as fd:
-        fd.write(r)
+url = "https://api.covid19api.com/summary"
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+result = json.loads(response.text)
+
+
+print(result["Global"])
+
+
+"""""
+r = requests.get("https://apiogrenme.free.beeceptor.com/users", stream=True)
+
+print(r.elapsed)
+
+with open("Result.txt", "wb") as fd:
+    fd.write(r.content)
+    fd.close()
+
+
+print(r.status_code)
+print(r.content)
+print(r.headers)
+headers = r.headers.decode("utf-8")
+
+with open("ResultHeaders.txt", "wb") as fd:
+    fd.write(headers)
+    fd.close()
+""" ""
